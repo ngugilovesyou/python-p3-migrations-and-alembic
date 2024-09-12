af2cffd91bd3c8e5f710200d8e3ed704f1cd2786
samuelgitau <amsamgitau@gmail.com>
2024-09-12 19:57:04 +0300

    complete lab

---
+++ app/migrations/versions/3d934cff8d2e_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 3d934cff8d2e
+Revises: 
+Create Date: 2024-09-12 19:49:03.371634
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '3d934cff8d2e'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
